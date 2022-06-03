c9568a84e7d5082f012343324f0cbe1d31c124af
Divyansh <divyanshsinghds2611@gmail.com>
2022-06-03 09:27:51 +0530

    auth p1 commit

--- app/src/main/java/com/example/just_click/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/just_click/ui/notifications/NotificationsFragment.java
@@ -1,5 +1,6 @@
 package com.example.just_click.ui.notifications;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,11 +15,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.just_click.R;
 import com.example.just_click.databinding.FragmentNotificationsBinding;
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class NotificationsFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
     private FragmentNotificationsBinding binding;
+    private GoogleSignInClient mGoogleSignInClient;
+    private final static int RC_SIGN_IN =123;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -29,15 +36,32 @@ public class NotificationsFragment extends Fragment {
         View root = binding.getRoot();
 
 //        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(),
+                new Observer<String>() {
+                    @Override
+                    public void onChanged(@Nullable String s) {
 //                textView.setText(s);
-            }
-        });
+                    }
+                    createRequest();
+                });
         return root;
     }
+          private void createRequest()
+          {
+              // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+              GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                      .requestEmail()
+                      .build();
 
+              mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+          }
+
+public void signIn()
+{
+    Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+    startActivityForResult(signInIntent, RC_SIGN_IN);
+}
     @Override
     public void onDestroyView() {
         super.onDestroyView();
